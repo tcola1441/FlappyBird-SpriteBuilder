926bc6f6ca6847859e1a2f7a15617769cc046a8f
Valentino Colagiovanni <ValentinoColagiovanni@Valentino-Colagiovannis-MacBook-Pro.local>
2014-11-04 21:36:04 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,7 +7,7 @@
 - (void)initialize
 {
     // your code here
-    character = (Character*)[[CCBReader load:@"Character"]];
+    character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild: character];
 }
 
